f830dc1e419ee21dba04c1842adec11d117fc074
Rishu Pandey <80598009+Rishurp@users.noreply.github.com>
2023-05-18 13:57:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,8 @@
     console.log("MongoDB is Connected....")
  }).catch(err=>console.log(err));
 
-app.use(cors());
-app.get("/", (req, res) > {
-res.setHeader("Access-Control-Allow-Credentials","true");
-res.send("Api is running...");
-});
-
-
 app.use("/api/users",userRoute);
 app.use("/api/pins",pinRoute);
-
 
  app.listen(8800,()=>{
  console.log("Backend Server is Running......")
