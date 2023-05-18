60ec5bcbf5c88b6df2e9e452d15fe6e2b57ba9d1
Rishu Pandey <80598009+Rishurp@users.noreply.github.com>
2023-05-19 00:07:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,19 @@
  dotenv.config();
 
  app.use(express.json());
+ app.use(function(req, res, next) {
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 
  mongoose.connect(process.env.MONGO_URL).then(()=>{
