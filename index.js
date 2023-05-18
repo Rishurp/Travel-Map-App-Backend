5df758872c353d635ed9d8776ccc65abdd5c55d2
Rishurp <rishupandey3102@gmail.com>
2023-05-18 12:55:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+ const express = require("express");
+ const mongoose =require("mongoose");
+ const dotenv =require("dotenv");
+ const app=express();
+ const pinRoute = require("./routes/pins");
+ const userRoute = require("./routes/users");
+
+
+ dotenv.config();
+
+ app.use(express.json());
+
+ mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("MongoDB is Connected....")
+ }).catch(err=>console.log(err));
+
+
+app.use("/api/users",userRoute);
+app.use("/api/pins",pinRoute);
+
+
+ app.listen(8800,()=>{
+ console.log("Backend Server is Running......")
+ })
---
+++ routes/users.js
@@ -0,0 +1,61 @@
+const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user._id);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    //find user
+    const user = await User.findOne({ username: req.body.username });
+    if(!user)
+    {
+       res.status(400);
+      return res.json("Wrong username or password");
+
+    }
+    //validate password
+    const validPassword =   bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if(!validPassword)
+    {
+       res.status(400);
+      return res.json("Wrong username or password");
+    }
+
+    
+     
+
+    //send response
+    res.status(200).json({ _id: user._id, username: user.username });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
